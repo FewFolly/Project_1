1eec77752472d47c96d20a5ee0abcfb92056e493
FewFolly <65776274+FewFolly@users.noreply.github.com>
2020-11-22 22:23:16 +0300

    done close and open modal window
    Написан код для открытия и закрытия модального окна в JS

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,16 @@
-console.log('Всем хай,я крыса');
+console.log('Всем хай,я скрипт');
+// Проверка загрузки документа
+document.addEventListener("DOMContentLoaded",function(event){
+    const modal = document.querySelector('.modal');
+    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+    const closeBtn = document.querySelector('.modal__close');
+    const switchModal = () => {
+        modal.classList.toggle('modal_visible');
+    };
+// Слушатель событий активации модального окна   
+modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+});
+// Слушатель событий для кнопки закрытия
+closeBtn.addEventListener('click', switchModal);
+});
