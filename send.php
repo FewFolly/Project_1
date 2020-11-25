77991b3380202b32b7ba06a35a657ce557d15d34
FewFolly <65776274+FewFolly@users.noreply.github.com>
2020-11-25 22:32:18 +0300

    add phpmailer
    Добавлен phpmailer
    Создана страница благодарности за регистрацию
    Добавлен якорь на основной файл index.html при нажатии на "Главная" в верхнем меню страницы благодарности thanks.html

---
+++ send.php
@@ -0,0 +1,40 @@
+<?php
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone'];
+
+
+// Load Composer's autoloader
+require 'PHP/phpmailer/PHPMailer.php';
+require 'PHP/phpmailer/SMTP.php';
+require 'PHP/phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'petrrprogramenko@gmail.com';                     // SMTP username
+    $mail->Password   = '47PETRkapustaCHEESE';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('petrrprogramenko@gmail.com', 'Petr');
+    $mail->addAddress('insane666social@gmail.com');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'New message from Project_1';
+    $mail->Body    = "Имя пользователя: ${userName},телефон пользователя: ${userPhone},почта пользователя: ${userEmail}";
+
+    $mail->send();
+    header('Location: thanks.html');
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
