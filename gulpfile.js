90ff390db854f3405832d28ae260a002a158820c
FewFolly <65776274+FewFolly@users.noreply.github.com>
2020-11-26 23:31:41 +0300

    add section design
    Сделана секция design
    Исправлен footer-bg
    Исправлен fonts.sass
    Сделан отрывок оптимизации в gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
+const { src } = require('gulp');
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -9,3 +12,8 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+// function buildCSS(done) {
+//     src('CSS/**/**.css').pipe(cleanCSS({compatibility: 'ie8'}).pipe(dest)
+//     done();
+// }
